12652a89fd1271af47e52c5108426f111eba7175
waibaBikash <kavrelibikash@gmail.com>
2024-05-31 23:58:51 +1000

    create a home page

---
+++ src/pages/Coin/Coin.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Coin.css';
+
+const Coin = () => {
+  return (
+    <div className='coin'>
+
+    </div>
+  )
+}
+
+export default Coin;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <div className="hero">
+         <h1>Largest <br /> Crypto Marketplace</h1>
+         <p>Welcome to the world's largest cryptocurrency marketplace. Sign up to explore more about cryptos.
+         </p>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
